eedd69a2fef8c5a4622d7c57da72cb94adc138f8
PaoPei01 <paopei2706@gmail.com>
2026-03-06 15:52:07 +0700

    Revert "Merge branch 'main' of https://github.com/jaonayzzz/ComPro-Project"
    This reverts commit 666ac0e6225f615db4de47b7534dcc463b3191a8, reversing
    changes made to 5cb12721d9a6064d19986f2ba9838876c032829d.

--- app_ready.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-extern bool startScreenReady;
-extern bool fontsLoaded;
-extern bool texturesLoaded;
-extern bool appReady;
-
-void updateAppReady();
--- ui_loading.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-#include "app_state.h"
-
-void initLoadingScreen();
-void updateLoadingScreen(AppState& state);
-void drawLoadingScreen(sf::RenderWindow& window);
